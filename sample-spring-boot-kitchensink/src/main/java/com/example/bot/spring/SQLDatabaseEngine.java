ee2ff4d24d859969d89fb1f56a37c9d16e0f8e82
LUNG Kun Hung <khlung@WIN2K.CS.UST.HK>
2017-09-26 15:51:37 +0800

    testing only

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -5,16 +5,45 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.sql.*;
 import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 
 @Slf4j
 public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
-		//Write your code here
+		String result = null;
+		try {
+			Connection con=getConnection();
+			PreparedStatement stmt = con.prepareStatement(
+			"SELECT * FROM testdb");
+			ResultSet rs = stmt.executeQuery();
+			while (result == null && rs.next()) {
+				if (text.toLowerCase().contains(rs.getString(1).toLowerCase())) {
+					result = rs.getString(2);
+				}
+			}
+			rs.close();
+		} catch (Exception e) {
+			log.info("Exception while reading db", e.toString());
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				if(con != null)
+					con.close();
+			} catch (Exception ex) {
+				log.info("Exception while closing connection", ex.toString());
+			}
+		}
+		if (result != null)
+			return result;
+		throw new Exception("NOT FOUND");
+    }
 		return null;
 	}
-	
 	
 	private Connection getConnection() throws URISyntaxException, SQLException {
 		Connection connection;
